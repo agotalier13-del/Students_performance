ece0b6230eb8e6c8e2fb60be979ae06a18408168
agotalier13-del <agotalier13@gmail.com>
2026-04-08 13:46:02 +0300

    Add files via upload

---
+++ Student Average.py
@@ -0,0 +1,20 @@
+#Subject:Maths, English,CRE
+#Assign variables ,user inputs
+#sum of subjects
+#number of subjects
+#average
+
+English=float(input("enter the value of English"))
+Maths=float(input("enter the value of Maths")) 
+CRE=float(input("enter the value of CRE"))
+sum_ofsubjects = English +Maths + CRE
+
+num_sub=3
+
+Average = sum/num_sub
+
+
+print("total_sum=",sum)
+print("total_average=",Average)
+
+
